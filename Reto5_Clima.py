7079570e0cd26b2c26805b5d320b563296f9fcf0
Jesus <104527491+FuryBaco@users.noreply.github.com>
2022-06-03 18:31:03 -0500

    Add files via upload

--- Reto5_Clima.py
+++ Reto5_Clima.py
@@ -1,17 +1,13 @@
 import json
 import pandas as pd
-
 data = pd.read_csv(r'data.csv')
-
 d_to_json = {}
 
 def promedio(location):
-    prom = list(data.loc[data['location'] == location].mean())
-    return float('{0:.1f}'.format(prom[-2])), float('{0:.1f}'.format(prom[-1]))
-
+    prom = list(data.loc[data['location'] == location][['temperature','pressure']].mean())
+    return float('{0:.1f}'.format(prom[0])), float('{0:.1f}'.format(prom[1]))
 
 locs = sorted(list(data['location'].unique()))
-
 for location in locs:
     d_to_json[str(location)] = promedio(location)
 
@@ -19,7 +15,6 @@ with open('promedios.json', 'w') as f:
     json.dump(d_to_json, f)
 
 data_nuevo = data
-
 data_nuevo['above_avg_temp'] = data_nuevo.apply(
     lambda n: 'SI' if n.temperature > d_to_json[str(n.location)][0] else 'NO'
     if n.temperature < d_to_json[str(n.location)][0] else 'IGUAL',
@@ -28,7 +23,6 @@ data_nuevo['above_avg_pres'] = data_nuevo.apply(
     lambda n: 'SI' if n.pressure > d_to_json[str(n.location)][1] else 'NO'
     if n.pressure < d_to_json[str(n.location)][1] else 'IGUAL',
     axis=1)
+print(data_nuevo)
 
 data_nuevo.to_csv(r'data_nuevo.csv', index=False)
-
-
